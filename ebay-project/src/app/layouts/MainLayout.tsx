d5e4f356681c889c92c620d3709a696d038447c4
João Víctor Tessarolo <83466473+joaotessarolo@users.noreply.github.com>
2024-05-04 13:33:19 -0300

    fix

--- ebay-project/src/app/layouts/MainLayout.tsx
+++ ebay-project/src/app/layouts/MainLayout.tsx
@@ -5,7 +5,11 @@ import MainHeader from "./includes/MainHeader";
 import SubMenu from "./includes/SubMenu";
 import TopMenu from "./includes/TopMenu";
 
-export default function MainLayout({ children }) {
+type MainLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <>
       <div id="MainLayout" className="min-w-[1050px] max-w-[1300px] mx-auto">
